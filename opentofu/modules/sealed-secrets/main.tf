4481ed69b12d6e18d5dcc1394443ddc838d29b6e
Mark Rhoades-Brown <mark@rhoades-brown.co.uk>
2023-12-02 21:10:18 +0000

    remove sealed secrets version - always install the latest

--- opentofu/modules/sealed-secrets/main.tf
+++ opentofu/modules/sealed-secrets/main.tf
@@ -2,7 +2,6 @@ resource "helm_release" "sealed-secrets" {
   name       = "sealed-secrets"
   repository = "https://bitnami-labs.github.io/sealed-secrets"
   chart      = "sealed-secrets"
-  version    = "v2.13.3"
   namespace  = "kube-system"
 
   set {
